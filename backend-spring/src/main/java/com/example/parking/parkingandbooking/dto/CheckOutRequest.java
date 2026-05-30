d028eb95229e1bf3c63e8588a91817505e3d33c7
your name <abc@gmail.com>
2026-05-30 20:52:49 +0700

    checkout

--- backend-spring/src/main/java/com/example/parking/parkingandbooking/dto/CheckOutRequest.java
+++ backend-spring/src/main/java/com/example/parking/parkingandbooking/dto/CheckOutRequest.java
@@ -1,5 +1,14 @@
 package com.example.parking.parkingandbooking.dto;
-
+import com.example.parking.parkingandbooking.dto.CheckOutRequest;
 public class CheckOutRequest {
 
+    private String licensePlate;
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
 }
