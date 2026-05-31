4d7ee85d33cae5d65e598c37e2ce349f416f6504
trihv2689-prog <trihv2689@ut.edu.vn>
2026-06-01 01:26:05 +0700

    Add incident type enum

---
+++ backend-spring/src/main/java/com/example/parking/incident/domain/model/enums/IncidentType.java
@@ -0,0 +1,10 @@
+package com.example.parking.incident.domain.model.enums;
+
+public enum IncidentType {
+    LOST_CARD,
+    VEHICLE_DAMAGE,
+    WRONG_SLOT,
+    PAYMENT_ERROR,
+    SYSTEM_ERROR,
+    OTHER
+}
